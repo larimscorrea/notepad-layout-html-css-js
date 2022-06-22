dab7b1a16cb289dceb2557ba99e98d67e2c7e253
Larissa <larimscorrea@gmail.com>
2022-06-21 23:42:10 -0300

    Added foooter

--- index.js
+++ index.js
@@ -2,3 +2,19 @@
 $('body').on('click', '.js-turnoff-btn', function() {
     $('.lamp-light').toggleClass('open');
 });
+
+
+$('body').on('click', '.js-turnoff-btn', function() {
+    $('.lamp-light-closed').reloadClass('closed');
+});
+
+/**function closedLight() {
+    if()
+}*/
+
+/*const $lampLight = document.querySelector('.lamp-light.open')
+const $lampWithout = document.querySelector('.js-turnoff-btn')
+
+function iluminationNow() {
+    if()
+}*/
